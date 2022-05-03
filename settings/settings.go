816cd277b08791c3fec001ea38047e49cf829be4
qiaoshurui <1635513903@qq.con>
2022-05-03 11:02:58 +0800

    GoWeb项目开发脚手架

---
+++ settings/settings.go
@@ -0,0 +1,27 @@
+package settings
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+
+	"github.com/spf13/viper"
+)
+
+func Init() (err error) {
+	viper.SetConfigName("config") // 配置文件名称(无扩展名)
+	viper.SetConfigType("yaml")   // 如果配置文件的名称中没有扩展名，则需要配置此项
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig() // 查找并读取配置文件
+	if err != nil {            // 处理读取配置文件的错误
+		fmt.Printf("viper.ReadInConfig() failed,err:%v\n", err)
+		return
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		// 配置文件发生变更之后会调用的回调函数
+		fmt.Println("配置文件被修改")
+	})
+	return
+
+}
